6580ac24baf4f59f6d652f5d7eb6cf29429f37c0
vieri12 <vieribachtiar15@gmail.com>
2022-06-28 03:50:50 +0700

    portfolio vieri

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4ade80',
+        dark: '#16a34a',
+      }
+    },
+  },
+  plugins: [],
+}
